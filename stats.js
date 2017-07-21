6321673c1c7e289168c3057876415cf76856f1c6
Yaroslav Martsynyuk <yaro@teacherspayteachers.com>
2017-07-21 17:57:03 +0300

    Count metrics

--- stats.js
+++ stats.js
@@ -4,14 +4,22 @@ var HOST = '127.0.0.1';
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
 
+var messages = {};
+
 server.on('listening', function () {
     var address = server.address();
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
 });
 
 server.on('message', function (message, remote) {
-    console.log(remote.address + ':' + remote.port +' - ' + message);
 
+	if (typeof messages[message] == 'undefined')
+		messages[message] = 0;
+	else
+		messages[message]++;
+
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+    console.log(messages);
 });
 
 server.bind(PORT, HOST);
