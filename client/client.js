93a95a5dc4aa56e90061a312390135bdbb2d8047
Yaroslav Martsynyuk <yaro@wise-engineering.com>
2017-07-26 21:33:34 +0300

    Basic server

---
+++ client/client.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require("body-parser");
+var exphbs = require('express-handlebars');
+var helmet = require('helmet');
+var fs = require('fs');
+var http = require('http');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(helmet());
+
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.get('/', function (req, res) {
+	res.render('index');
+});
+
+http.Server(app).listen(process.env.APP_HTTP_LISTEN || 3000, function() {
+	console.log('listening on *:3000');
+});
--- stats.js
+++ stats.js
@@ -1,25 +1,59 @@
-var PORT = 8190;
-var HOST = '127.0.0.1';
-
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
+var config = require('./config/config');
 
-var messages = {};
+var metrics = {};
 
-server.on('listening', function () {
-    var address = server.address();
-    console.log('UDP Server listening on ' + address.address + ":" + address.port);
-});
+function flushMetrics() {
+    // Aggregate and save metrics to backend
+    metrics = {};
+}
 
-server.on('message', function (message, remote) {
+function sanitizeStatName(stat) {
+    return stat.replace(/[^a-zA-Z_\-0-9\.]/g, '');
+}
 
-	if (typeof messages[message] == 'undefined')
-		messages[message] = 0;
-	else
-		messages[message]++;
+function saveMetric(message) {
+    var packet_data = message.toString();
 
-    console.log(remote.address + ':' + remote.port +' - ' + message);
-    console.log(messages);
-});
+    if (packet_data.indexOf(":") > -1 && (packet_data.split(":").length) == 3) {
+        var data = packet_data.split(":");
+    } else {
+        var data = [packet_data, '0 - 0', '1'];
+    }
 
-server.bind(PORT, HOST);
+    var key = data[0];
+    var coords = data[1];
+    var value = data[2];
+
+    if (value.indexOf("\n") > -1) {
+        value = value.split("\n")[0];
+    }
+
+    var key = sanitizeStatName(key);
+    value = Number(value);
+
+    if (typeof metrics[key] == 'undefined') {
+        metrics[key] = {};
+    }
+
+    if (typeof metrics[key][coords] == 'undefined') {
+        metrics[key][coords] = value;
+    } else {
+        metrics[key][coords] += value;
+    }
+
+    console.log(metrics);
+}
+
+function startServer() {
+    server.on('message', function (message, remote) {
+        saveMetric(message);
+    });
+
+    server.bind(config.port || 8190, config.host || '127.0.0.1');
+
+    setInterval(flushMetrics, config.flushInterval);
+}
+
+startServer();
