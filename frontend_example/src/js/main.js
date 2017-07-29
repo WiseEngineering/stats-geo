f9b059ad1a05838eb4edda84a22eb6366b65ae05
Yaroslav Martsynyuk <yaro@wise-engineering.com>
2017-07-29 15:26:47 +0300

    Parse metrict on frontend

--- frontend_example/src/js/main.js
+++ frontend_example/src/js/main.js
@@ -5,12 +5,26 @@ require('jquery');
 require('./vector-map.min.js');
 require('./world-mill.js');
 
+function drawMarker(lat, lon, value) {
+
+}
+
 $(document).ready(function(){
     $('#world-map').vectorMap({map: 'world_mill'});
 
     var socket = io();
 
     socket.on('stats', function(msg){
-        console.log(msg);
+        var stats = JSON.parse(msg);
+
+        for (metric in stats) {
+            for (coords in stats[metric]) {
+                drawMarker(
+                    parseFloat(coords.split(",")[0]),
+                    parseFloat(coords.split(",")[1]),
+                    stats[metric][coords].value
+                );
+            }
+        }
     });
 });
