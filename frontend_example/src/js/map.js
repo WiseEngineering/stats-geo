d0f862e4a420cea64c8adc000d5ca08441b42600
Your Name <you@example.com>
2017-07-29 17:17:59 +0300

    add map

--- frontend_example/src/js/map.js
+++ frontend_example/src/js/map.js
@@ -5,7 +5,7 @@ define([], () => {
         init: () => {
             return new google.maps.Map(document.getElementById('map'), {
                 center: {lat: 0, lng: 0},
-                zoom: 3
+                zoom: 1
             })
         },
 
