b503c308bb75bcc7dfd6feb829a71d2c660d0daf
Yaroslav Martsynyuk <yaro@wise-engineering.com>
2017-09-04 11:35:06 +0300

    Rnd sh

--- rnd_geo_usa.sh
+++ rnd_geo_usa.sh
@@ -4,9 +4,9 @@
 while [  True ]; do
 	long=$(($RANDOM % 52 - 124))"."$(($RANDOM % 32767))
 	lat=$(($RANDOM % 23 + 26))"."$(($RANDOM % 32767))
-	metric_value=$(($RANDOM % 50))
+	metric_value=$(($RANDOM % 100))
 
-	sleep_time=$(($RANDOM % 2))
+	sleep_time=$(($RANDOM % 1))
 	sleep $sleep_time
 
 	#echo "purchase|$lat,$long|$metric_value@0.1"
